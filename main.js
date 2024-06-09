9aec3cde1bd8689391a6ade785eac9ae28af45dc
Taher Shabbir <taherihh53@gmail.com>
2024-06-09 03:17:59 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+//assignment no 5
+var famQoute = "A person who never made a mistake never tried anything new.";
+var famousPerson = "Albert Einstein";
+var message = "".concat(famousPerson, " once said, \"").concat(famQoute, "\"");
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,8 @@
+//assignment no 5
+
+let famQoute:string = "A person who never made a mistake never tried anything new.";
+
+let famousPerson:string ="Albert Einstein";
+let message:string = `${famousPerson} once said, "${famQoute}"`;
+
+console.log(message);
